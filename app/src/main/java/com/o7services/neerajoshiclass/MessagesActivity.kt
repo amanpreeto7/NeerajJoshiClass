58dc68c09807eef811c6cb75d4d7abb80bce4206
Amanpreet Kaur <69182131+amanpreeto7@users.noreply.github.com>
2023-12-04 11:22:38 +0530

    Alert Dialog Added

--- app/src/main/java/com/o7services/neerajoshiclass/MessagesActivity.kt
+++ app/src/main/java/com/o7services/neerajoshiclass/MessagesActivity.kt
@@ -4,17 +4,21 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import com.google.android.material.snackbar.Snackbar
 
 class MessagesActivity : AppCompatActivity() {
     var btnShowToast : Button?= null
     var btnShowSnackbar : Button?= null
+    var btnAlertDialog : Button ?= null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_messages)
 
         btnShowToast = findViewById(R.id.btnShowToast)
         btnShowSnackbar = findViewById(R.id.btnShowSnackbar)
+        btnAlertDialog = findViewById(R.id.btnAlertDialog)
+
         btnShowToast?.setOnClickListener{
             Toast.makeText(this, "This is text message", Toast.LENGTH_SHORT).show()
         }
@@ -25,6 +29,30 @@ class MessagesActivity : AppCompatActivity() {
                     btnShowSnackbar?.setText("Updated After ok button Click")
                 }
                 .show()
+        }
+
+        btnAlertDialog?.setOnClickListener{
+            //Alert dialog class in-build
+            //Features, attributes
+            //Title, message
+            //List, single choice, mutli choice
+            //List we cannot show message
+            //3 buttons - Positive, negative, neutral
+            //activity context this
+            var alertDialog = AlertDialog.Builder(this)
+            alertDialog.setTitle("This is title")
+            alertDialog.setMessage("This is message")
+            alertDialog.setCancelable(false)
+            alertDialog.setPositiveButton("Positive", {alertDialog,buttonPressed->
+                Toast.makeText(this, "Positve button clicked", Toast.LENGTH_SHORT).show()
+            })
+            alertDialog.setNegativeButton("Negative", {_,_->
+                Toast.makeText(this, "Negative button clicked", Toast.LENGTH_SHORT).show()
+            })
+            alertDialog.setNeutralButton("Neutral", {_,_->
+                Toast.makeText(this, "Neutral button clicked", Toast.LENGTH_SHORT).show()
+            })
+            alertDialog.show()
         }
 
     }
